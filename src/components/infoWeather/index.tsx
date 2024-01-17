68c06a2fd1244fc8f5080c11df8f317f53f5065f
Andre Lusegardis <lusegardis@gmail.com>
2024-01-17 19:25:41 -0300

    layout and colors are now alined with theexample image

--- src/components/infoWeather/index.tsx
+++ src/components/infoWeather/index.tsx
@@ -1,15 +1,10 @@
 import { useCtxForecast } from "@/context/ctxForecast";
 import { useCtxWeather } from "@/context/ctxWeather";
-import { useState } from "react";
 import styles from "./infoWeather.module.css";
 import svgSun from "@/icons/sun.svg";
 import Image from "next/image";
 
 export const InfoWeather: React.FC = () => {
-  const [coordinates, setCoordinates] = useState<{
-    latitude: number;
-    longitude: number;
-  } | null>(null);
   const { ctxWeather, setCtxWeather } = useCtxWeather();
   const { ctxTomorrow, setCtxTomorrow, ctxAfterTomorrow, setCtxAfterTomorrow } =
     useCtxForecast();
@@ -19,23 +14,29 @@ export const InfoWeather: React.FC = () => {
       {typeof ctxWeather?.main !== "undefined" && (
         <div>
           <div
-            className={
-              ctxWeather.main.temp < 15
-                ? `${styles.todayWeatherBlue}`
-                : ctxWeather.main.temp > 35
-                ? `${styles.todayWeatherRed}`
-                : `${styles.todayWeatherYellow}`
-            }
+            className={`${styles.dayData} 
+              ${
+                ctxWeather.main.temp < 15
+                  ? `${styles.todayWeatherBlue}`
+                  : ctxWeather.main.temp > 35
+                  ? `${styles.todayWeatherRed}`
+                  : `${styles.todayWeatherYellow}`
+              }`}
           >
-            <Image
-              src={svgSun}
-              alt="compass icon"
-              className={`${styles.icon}`}
-            />
-            <div>
+            <div className={`${styles.imgBlock}`}>
+              <Image
+                src={svgSun}
+                alt="compass icon"
+                className={`${styles.icon}`}
+              />
+            </div>
+            <div className={`${styles.todayInfo}`}>
               <p>HOJE</p>
               <p>{ctxWeather.main.temp}ºC</p>
-              <p>{ctxWeather.weather[0].description}</p>
+              <p className={`${styles.todayDescription}`}>
+                {ctxWeather.weather[0].description.charAt(0).toUpperCase() +
+                  ctxWeather.weather[0].description.slice(1)}
+              </p>
               <p>Vento: NO {ctxWeather.wind.speed}km/h</p>
               <p>Humidade: {ctxWeather.main.humidity}%</p>
             </div>
@@ -43,31 +44,39 @@ export const InfoWeather: React.FC = () => {
 
           {ctxTomorrow !== null && (
             <div
-              className={
+              className={`${styles.tomorrowData} ${
                 ctxTomorrow < 15
                   ? `${styles.tomorrowWeatherBlue}`
                   : ctxTomorrow > 35
                   ? `${styles.tomorrowWeatherRed}`
                   : `${styles.tomorrowWeatherYellow}`
-              }
+              }`}
             >
-              <p>AMANHÃ</p>
-              <p>{ctxTomorrow}ºC</p>
+              <div className={`${styles.dayData}`}>
+                <div className={`${styles.infoSides}`}>&nbsp;</div>
+                <div className={`${styles.infoSides}`}>
+                  <p>AMANHÃ</p>
+                  <p>{ctxTomorrow}ºC</p>
+                </div>
+              </div>
             </div>
           )}
 
           {ctxAfterTomorrow !== null && (
             <div
-              className={
+              className={`${styles.dayData} ${
                 ctxAfterTomorrow < 15
                   ? `${styles.afterTomorrowWeatherBlie}`
                   : ctxAfterTomorrow > 35
                   ? `${styles.afterTomorrowWeatherRed}`
                   : `${styles.afterTomorrowWeatherYellow}`
-              }
+              }`}
             >
-              <p>DEPOIS DE AMANHÃ</p>
-              <p>{ctxAfterTomorrow}ºC</p>
+              <div className={`${styles.infoSides}`}>&nbsp;</div>
+              <div className={`${styles.infoSides}`}>
+                <p>DEPOIS DE AMANHÃ</p>
+                <p>{ctxAfterTomorrow}ºC</p>
+              </div>
             </div>
           )}
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <div className={`${styles.mainData}`}>
-          <BingBackground />
+        <BingBackground />
+        <div className={`${styles.dataBlock}`}>
           <InfoInput />
           <InfoWeather />
         </div>
