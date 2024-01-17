043d124c49609806075b416d0a0e87cb93fdaf8c
Andre Lusegardis <lusegardis@gmail.com>
2024-01-17 05:32:30 -0300

    added the background color for infoWeather

--- src/components/infoWeather/index.tsx
+++ src/components/infoWeather/index.tsx
@@ -1,4 +1,3 @@
-import { useCtxCityState } from "@/context/ctxCityState";
 import { useCtxForecast } from "@/context/ctxForecast";
 import { useCtxWeather } from "@/context/ctxWeather";
 import { useState } from "react";
@@ -19,28 +18,58 @@ export const InfoWeather: React.FC = () => {
     <div className={`${styles.main}`}>
       {typeof ctxWeather?.main !== "undefined" && (
         <div>
-          <div>
+          <div
+            className={
+              ctxWeather.main.temp < 15
+                ? `${styles.todayWeatherBlue}`
+                : ctxWeather.main.temp > 35
+                ? `${styles.todayWeatherRed}`
+                : `${styles.todayWeatherYellow}`
+            }
+          >
             <Image
               src={svgSun}
               alt="compass icon"
               className={`${styles.icon}`}
             />
-            <div className={`${styles.weatherToday}`}>
+            <div>
               <p>HOJE</p>
               <p>{ctxWeather.main.temp}ºC</p>
               <p>{ctxWeather.weather[0].description}</p>
-              <p>Vento: {ctxWeather.wind.speed}km/h</p>
+              <p>Vento: NO {ctxWeather.wind.speed}km/h</p>
               <p>Humidade: {ctxWeather.main.humidity}%</p>
             </div>
           </div>
-          <div>
-            <p>AMANHÃ</p>
-            <p>{ctxTomorrow}ºC</p>
-          </div>
-          <div>
-            <p>DEPOIS DE AMANHÃ</p>
-            <p>{ctxAfterTomorrow}ºC</p>
-          </div>
+
+          {ctxTomorrow !== null && (
+            <div
+              className={
+                ctxTomorrow < 15
+                  ? `${styles.tomorrowWeatherBlue}`
+                  : ctxTomorrow > 35
+                  ? `${styles.tomorrowWeatherRed}`
+                  : `${styles.tomorrowWeatherYellow}`
+              }
+            >
+              <p>AMANHÃ</p>
+              <p>{ctxTomorrow}ºC</p>
+            </div>
+          )}
+
+          {ctxAfterTomorrow !== null && (
+            <div
+              className={
+                ctxAfterTomorrow < 15
+                  ? `${styles.afterTomorrowWeatherBlie}`
+                  : ctxAfterTomorrow > 35
+                  ? `${styles.afterTomorrowWeatherRed}`
+                  : `${styles.afterTomorrowWeatherYellow}`
+              }
+            >
+              <p>DEPOIS DE AMANHÃ</p>
+              <p>{ctxAfterTomorrow}ºC</p>
+            </div>
+          )}
         </div>
       )}
     </div>
